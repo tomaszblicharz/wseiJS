338e588acd5649e0e31a4da07901cf6e91e75651
tomaszblicharz <t.blicharz97@gmail.com>
2019-11-12 18:56:08 +0100

    next

--- Photoshop/canvas-paint.js
+++ Photoshop/canvas-paint.js
@@ -49,3 +49,32 @@ function brightnesFilter(amount = 10) //-rozjasnia pixele i przepisuje na nowo d
     }
     ctx.putImageData(canvasData, 0, 0)
 }
+class Photoshop {
+    constructor(targetElementId) {
+        this.canvas = document.querySelector('#' + targetElementId)
+        this.canvas.addEventListener('touchmove', (e) => this.onTouchMove(e))
+        this.canvas.addEventListener('touchstart', (e) => this.onTouchMove(e))
+        canvas.addEventListener('touchend', onTouchEnd)
+        this.brushShapeName = 'square';
+
+
+    }
+
+    onTouchMove(e) {
+        // pobierz pędzel
+        this.brushShape = new Brush(this.brushShapeName)
+        const brush = this.brushShape.getBrush();
+        // ustal położenie
+        let x = e.touches[0].clientX - this.canvas.offsetLeft;
+        let y = e.touches[0].clientY - this.canvas.offsetTop;
+        brush.style.top = `${y}px`;
+        brush.style.left = `${x}px`;
+        //namaluj
+        this.canvas.appendChild(brush);
+    }
+
+    setBrush(brushShape) {
+        this.brushShapeName = brushShape;
+
+    }
+}
--- Photoshop/main.js
+++ Photoshop/main.js
@@ -1,42 +1,5 @@
 document.addEventListener('DOMContentLoaded', appStart);
 
-// let brushType = 'square';
-// function appStart(){
-//     canvas = document.querySelector('#canvas');
-//     canvas.addEventListener('touchstart', onTouchStart)
-//     canvas.addEventListener('touchend', onTouchEnd)
-//     canvas.addEventListener('touchmove', onTouchMove)
-
-//     document
-//         .querySelector('#squareBtn')
-//         .addEventListener('click', () => changeBrush('square'))
-
-//     document
-//         .querySelector('#circleBtn')
-//         .addEventListener('click' , () => changeBrush('circle'))
-// }
-
-// function changeBrush(typeBrush){
-//     brushType = typeBrush;
-// }
-
-// function onTouchMove(e){
-//     // pobierz pędzel
-//     const brush = getBrush();
-//     // ustal położenie
-//     let x = e.touches[0].clientX - canvas.offsetLeft;
-//     let y = e.touches[0].clientY - canvas.offsetTop;
-//     brush.style.top = `${y}px`;
-//     brush.style.left =`${x}px`;
-//     //namaluj
-//     canvas.appendChild(brush);
-// }
-
-// function getBrush(){
-//     const div = document.createElement('div');
-//     div.classList.add('brush', `${brushType}-brush`);
-//     return div;
-// }
 
 function appStart() {
     const myPs = new Photoshop('canvas');
