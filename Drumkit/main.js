d4fe5114bda6b95c9c919b6365cb5dca3879c29b
tomaszblicharz <t.blicharz97@gmail.com>
2019-11-24 19:58:57 +0100

    work

--- Drumkit/main.js
+++ Drumkit/main.js
@@ -54,8 +54,8 @@ function Buttons(id, x) {
 }
 
 
-
 //Kanal 1
+
 function play(e) {
     if (record1Active) {
         table1.push({
@@ -63,39 +63,60 @@ function play(e) {
             code: e.code,
         })
     }
-    playSound1(e.code);
-}
-
-function startRecord1() {
-    startRecord1Time = Date.now();
-    record1Active = true;
-}
-
-
-function stopRecord1() {
-    record1Active = false;
-}
-
-function playRecord1() {
-    for (let i = 0; i < table1.length; i++) {
-        setTimeout(playSound1, table1[i].time - startRecord1Time, table1[i].code);
-    }
-}
-
-//Kanal 2
-function play(e) {
     if (record2Active) {
         table2.push({
             time: Date.now(),
             code: e.code,
         })
     }
-    playSound2(e.code);
+    if (record3Active) {
+        table3.push({
+            time: Date.now(),
+            code: e.code,
+        })
+    }
+    if (record4Active) {
+        table4.push({
+            time: Date.now(),
+            code: e.code,
+        })
+    }
+    playSound(e.code);
 }
+
+function startRecord1() {
+    startRecord1Time = Date.now();
+    record1Active = true;
+    table1 = [];
+    record1.disabled = true;
+    stopRecord1.disabled = false;
+    playRecord1.disabled = false;
+}
+
+
+function stopRecord1() {
+    record1Active = false;
+    record1.disabled = false;
+    stopRecord1.disabled = true;
+    playRecord1.disabled = false;
+}
+
+function playRecord1() {
+    if (!record1Active) {
+        for (let i = 0; i < table1.length; i++) {
+            setTimeout(playSound, table1[i].time - startRecord1Time, table1[i].code);
+        }
+    }
+    record1.disabled = false;
+    stopRecord1.disabled = false;
+    playRecord1.disabled = true;
+}
+//Kanal 2
 
 function startRecord2() {
     startRecord2Time = Date.now();
     record2Active = true;
+    table2 = [];
 }
 
 
@@ -104,25 +125,19 @@ function stopRecord2() {
 }
 
 function playRecord2() {
-    for (let i = 0; i < table2.length; i++) {
-        setTimeout(playSound2, table2[i].time - startRecord2Time, table2[i].code);
+    if (!record2Active) {
+        for (let i = 0; i < table2.length; i++) {
+            setTimeout(playSound, table2[i].time - startRecord2Time, table2[i].code);
+        }
     }
 }
 
 //Kanal 3
-function play(e) {
-    if (record3Active) {
-        table3.push({
-            time: Date.now(),
-            code: e.code,
-        })
-    }
-    playSound3(e.code);
-}
 
 function startRecord3() {
     startRecord3Time = Date.now();
     record3Active = true;
+    table3 = [];
 }
 
 
@@ -131,25 +146,19 @@ function stopRecord3() {
 }
 
 function playRecord3() {
-    for (let i = 0; i < table3.length; i++) {
-        setTimeout(playSound3, table3[i].time - startRecord3Time, table3[i].code);
+    if (!record3Active) {
+        for (let i = 0; i < table3.length; i++) {
+            setTimeout(playSound, table3[i].time - startRecord3Time, table3[i].code);
+        }
     }
 }
-
 //Kanal 4
-function play(e) {
-    if (record4Active) {
-        table4.push({
-            time: Date.now(),
-            code: e.code,
-        })
-    }
-    playSound4(e.code);
-}
+
 
 function startRecord4() {
     startRecord4Time = Date.now();
     record4Active = true;
+    table4 = [];
     record4.disabled = true;
     stopRecord4.disabled = false;
     playRecord4.disabled = false;
@@ -164,12 +173,14 @@ function stopRecord4() {
 }
 
 function playRecord4() {
-    for (let i = 0; i < table4.length; i++) {
-        setTimeout(playSound4, table4[i].time - startRecord4Time, table4[i].code);
+    if (!record4Active) {
+        for (let i = 0; i < table4.length; i++) {
+            setTimeout(playSound, table4[i].time - startRecord4Time, table4[i].code);
+        }
+        record4.disabled = false;
+        stopRecord4.disabled = false;
+        playRecord4.disabled = true;
     }
-    record4.disabled = false;
-    stopRecord4.disabled = false;
-    playRecord4.disabled = true;
 }
 
 function playAllRecords() {
@@ -179,130 +190,7 @@ function playAllRecords() {
     playRecord4();
 }
 
-function playSound1(key) {
-    switch (key) {
-        case 'KeyA':
-            clapSound.currentTime = 0;
-            clapSound.play();
-            break
-        case 'KeyS':
-            boomSound.currentTime = 0;
-            boomSound.play();
-            break
-        case "KeyD":
-            hihatSound.currentTime = 0;
-            hihatSound.play();
-            break
-        case 'KeyF':
-            kickSound.currentTime = 0;
-            kickSound.play();
-            break
-        case "KeyG":
-            openhatSound.currentTime = 0;
-            openhatSound.play();
-            break
-        case 'KeyH':
-            rideSound.currentTime = 0;
-            rideSound.play();
-            break
-        case 'KeyJ':
-            snareSound.currentTime = 0;
-            snareSound.play();
-            break
-        case "KeyK":
-            tinkSound.currentTime = 0;
-            tinkSound.play();
-            break
-        case "KeyL":
-            tomSound.currentTime = 0;
-            tomSound.play();
-            break
-    }
-}
-
-function playSound2(key) {
-    switch (key) {
-        case 'KeyA':
-            clapSound.currentTime = 0;
-            clapSound.play();
-            break
-        case 'KeyS':
-            boomSound.currentTime = 0;
-            boomSound.play();
-            break
-        case "KeyD":
-            hihatSound.currentTime = 0;
-            hihatSound.play();
-            break
-        case 'KeyF':
-            kickSound.currentTime = 0;
-            kickSound.play();
-            break
-        case "KeyG":
-            openhatSound.currentTime = 0;
-            openhatSound.play();
-            break
-        case 'KeyH':
-            rideSound.currentTime = 0;
-            rideSound.play();
-            break
-        case 'KeyJ':
-            snareSound.currentTime = 0;
-            snareSound.play();
-            break
-        case "KeyK":
-            tinkSound.currentTime = 0;
-            tinkSound.play();
-            break
-        case "KeyL":
-            tomSound.currentTime = 0;
-            tomSound.play();
-            break
-    }
-}
-
-function playSound3(key) {
-    switch (key) {
-        case 'KeyA':
-            clapSound.currentTime = 0;
-            clapSound.play();
-            break
-        case 'KeyS':
-            boomSound.currentTime = 0;
-            boomSound.play();
-            break
-        case "KeyD":
-            hihatSound.currentTime = 0;
-            hihatSound.play();
-            break
-        case 'KeyF':
-            kickSound.currentTime = 0;
-            kickSound.play();
-            break
-        case "KeyG":
-            openhatSound.currentTime = 0;
-            openhatSound.play();
-            break
-        case 'KeyH':
-            rideSound.currentTime = 0;
-            rideSound.play();
-            break
-        case 'KeyJ':
-            snareSound.currentTime = 0;
-            snareSound.play();
-            break
-        case "KeyK":
-            tinkSound.currentTime = 0;
-            tinkSound.play();
-            break
-        case "KeyL":
-            tomSound.currentTime = 0;
-            tomSound.play();
-            break
-    }
-}
-
-function playSound4(key) {
+function playSound(key) {
     switch (key) {
         case 'KeyA':
             clapSound.currentTime = 0;
